b2bbea58b4fa11c5f1a69df067aa0b2aa91e9be0
arturyyys <arturyyys@gmail.com>
2022-04-25 22:04:22 +0300

    Saying Hello with C#

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GradeBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, {args[1]}!");
+        }
+    }
+}
+
+// comment
