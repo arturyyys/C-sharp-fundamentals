a31bf120f25b832ad7054e165d9221e5b34c7212
arturyyys <arturyyys@gmail.com>
2022-04-26 21:31:39 +0300

    Adding Numbers

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,12 +6,18 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var x = 34.1;
+            var y = 10.4;
+            var result = x + y;
+            Console.WriteLine(result);
+
+
             if(args.Length > 0)
             {
             Console.WriteLine($"Hello, {args[0]}!");
             }
             else {
-                System.Console.WriteLine("Hello!");
+                Console.WriteLine("Hello!");
             }
         }
     }
