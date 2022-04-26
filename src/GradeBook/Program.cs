97ea30ffa8c6b9f21d0bfbd52c2ecf6ea2508895
arturyyys <arturyyys@gmail.com>
2022-04-26 22:21:22 +0300

    List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,27 +6,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            // var numbers = new double [3];
-            // numbers[0] = 12.7;
-            // numbers[1] = 10.3;
-            // numbers[2] = 6.11;
-
-            // var result = numbers[0];
-            // result = result + numbers[1];
-            // result = result + numbers[2];
-            // Console.WriteLine(result);
-
             var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
 
-            // var result = numbers[0];
-            // result += numbers[1];
-            // result += numbers[2];
-            // Console.WriteLine(result);
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
-                
+                result += number;
             }
             Console.WriteLine(result);
 
