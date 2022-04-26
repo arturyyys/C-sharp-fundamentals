a6ddaa3f8cfe0fe5a59fd65bb0a1686d52bf6db5
arturyyys <arturyyys@gmail.com>
2022-04-26 21:49:47 +0300

    Looping through Arrays

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,30 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var x = 34.1;
-            var y = 10.4;
-            var result = x + y;
+            // var numbers = new double [3];
+            // numbers[0] = 12.7;
+            // numbers[1] = 10.3;
+            // numbers[2] = 6.11;
+
+            // var result = numbers[0];
+            // result = result + numbers[1];
+            // result = result + numbers[2];
+            // Console.WriteLine(result);
+
+            var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
+
+            // var result = numbers[0];
+            // result += numbers[1];
+            // result += numbers[2];
+            // Console.WriteLine(result);
+
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                
+            }
             Console.WriteLine(result);
+
 
 
             if(args.Length > 0)
