17e4a1ffbfd48c774f1b2069c9ae57bff6987143
arturyyys <arturyyys@gmail.com>
2022-04-27 21:18:38 +0300

    Computing Statistics

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,24 +9,25 @@
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
+            // book.ShowStatistics();
             
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
             var result = 0.0;
             var highGrade = double.MinValue;
-            // var lowestGrade = double.MaxValue;
+            var lowGrade = double.MaxValue;
             foreach(var number in grades)
             {
                 highGrade = Math.Max(number, highGrade);
-                // lowestGrade = Math.Min(number, lowestGrade);
+                lowGrade = Math.Min(number, lowGrade);
                 result += number;
             }
             
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N1}");
-            System.Console.WriteLine(highGrade);
-            // System.Console.WriteLine(lowestGrade);
+            System.Console.WriteLine($"The highest grade is {highGrade}");
+            System.Console.WriteLine($"The lowest grade is {lowGrade}");
         }
     }
 }
