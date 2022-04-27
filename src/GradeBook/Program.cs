e69c6d01fae47a16634ecb3a226863d8e9aff958
arturyyys <arturyyys@gmail.com>
2022-04-27 20:43:06 +0300

    Requiring Constructor Parameters

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,8 +5,9 @@
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
             
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
