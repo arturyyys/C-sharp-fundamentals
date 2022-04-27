fcfd28b90b4dc4c017a636dfbeb29563dc703633
arturyyys <arturyyys@gmail.com>
2022-04-27 21:27:22 +0300

    Solving the Statistic Chalenge

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -13,7 +13,26 @@ namespace GradeBook
         {
             grades.Add(grade);
         }
-        private List<double> grades;
-        private string name;
+    
+  public void ShowStatistics()
+  {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+            System.Console.WriteLine($"The highest grade is {highGrade}");
+            System.Console.WriteLine($"The lowest grade is {lowGrade}");
+  }
+
+    private List<double> grades;
+    private string name;
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,25 +9,7 @@
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            // book.ShowStatistics();
-            
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-            
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            System.Console.WriteLine($"The highest grade is {highGrade}");
-            System.Console.WriteLine($"The lowest grade is {lowGrade}");
+            book.ShowStatistics();
         }
     }
 }
