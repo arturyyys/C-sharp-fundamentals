729c4c21609ff1790ad18dea49d0227b82bf4be3
arturyyys <arturyyys@gmail.com>
2022-04-27 20:17:26 +0300

    Adding a Constructor

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,6 +4,10 @@ namespace GradeBook
 {
     class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
