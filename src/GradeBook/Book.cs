d36f28410f8016ce5f49b8b7da14d453653154dd
arturyyys <arturyyys@gmail.com>
2022-04-28 21:18:22 +0300

    Creating a Unit Test Project

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,38 +1,36 @@
-using System.Collections.Generic;
-
 namespace GradeBook
 {
-    class Book
-    {
-        public Book(string name)
-        {
-            grades = new List<double>();
-            this.name = name;
-        }
-        public void AddGrade(double grade)
-        {
-            grades.Add(grade);
-        }
-    
-  public void ShowStatistics()
+ class Book
+ {
+  public Book(string name)
   {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-            
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            System.Console.WriteLine($"The highest grade is {highGrade}");
-            System.Console.WriteLine($"The lowest grade is {lowGrade}");
+   grades = new List<double>();
+   this.name = name;
+  }
+  public void AddGrade(double grade)
+  {
+   grades.Add(grade);
   }
 
-    private List<double> grades;
-    private string name;
-    }
+  public void ShowStatistics()
+  {
+   var result = 0.0;
+   var highGrade = double.MinValue;
+   var lowGrade = double.MaxValue;
+   foreach (var number in grades)
+   {
+    highGrade = Math.Max(number, highGrade);
+    lowGrade = Math.Min(number, lowGrade);
+    result += number;
+   }
+
+   result /= grades.Count;
+   Console.WriteLine($"The average grade is {result:N1}");
+   System.Console.WriteLine($"The highest grade is {highGrade}");
+   System.Console.WriteLine($"The lowest grade is {lowGrade}");
+  }
+
+  private List<double> grades;
+  private string name;
+ }
 }
