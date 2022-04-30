688669ccbf8b8154a1762dcbacbd0541619ca3c9
arturyyys <arturyyys@gmail.com>
2022-04-30 03:44:24 +0300

    Writing and Running Test

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -8,6 +8,16 @@ public class UnitTest1
   [Fact]
   public void Test1()
   {
+    // triple a = arrange => act => assert
+    // arrange section
+    var x = 5;
+    var y = 2;
+    var actual = x + y;
 
+    // act section
+    var expected = 7;
+
+    // assert section
+    Assert.Equal(expected, actual);
   }
 }
