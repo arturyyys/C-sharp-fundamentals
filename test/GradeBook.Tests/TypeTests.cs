05a58110ed4492146264f519c28b9d08c7df84e8
arturyyys <arturyyys@gmail.com>
2022-05-01 18:58:52 +0300

    CSharpIsPassByValue

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,12 +6,26 @@ namespace GradeBook.Tests;
 public class TypeTests
 {
    [Fact]
-   public void Test1()
+   public void CSharpIsPassByValue()
+   {
+      var book1 = GetBook("Book 1");
+      GetBookSetName(book1, "New Name");
+
+      Assert.Equal("Book 1", book1.Name);
+   }
+
+   private void GetBookSetName(Book book, string name)
+   {
+      book = new Book(name);
+   }
+
+   [Fact]
+   public void CanSetNameFromReference()
    {
       var book1 = GetBook("Book 1");
       SetName(book1, "New Name");
 
-      Assert.Equal("Book 1", book1.Name);
+      Assert.Equal("New Name", book1.Name);
    }
 
    private void SetName(Book book, string name)
