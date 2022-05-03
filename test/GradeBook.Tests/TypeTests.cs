c20dd9d8751ed8877ba3b79cd17d95573233b7ac
arturyyys <arturyyys@gmail.com>
2022-05-03 22:03:04 +0300

    The special case off strings in .NET

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -69,6 +69,21 @@ public class TypeTests
    }
 
    [Fact]
+   public void StringsBehaveLikeValueTypes()
+   {
+      string name = "Scott";
+      var upper = MakeUppercase(name);
+
+      Assert.Equal("Scott", name);
+      Assert.Equal("SCOTT", upper);
+   }
+
+   private string MakeUppercase(string parameter)
+   {
+      return parameter.ToUpper();
+   }
+
+   [Fact]
    public void GetBookReturnDifferentObjects()
    {
       var book1 = GetBook("Book 1");
