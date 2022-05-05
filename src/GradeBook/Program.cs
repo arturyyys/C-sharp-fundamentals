44302f124713974059605db6e9d631b8c80852d0
arturyyys <arturyyys@gmail.com>
2022-05-05 21:10:22 +0300

    Taking user input from the Console

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,14 +6,31 @@
       static void Main(string[] args)
       {
          var book = new Book("Scott's Grade Book");
-         book.AddGrade(89.1);
-         book.AddGrade(90.5);
-         book.AddGrade(77.5);
+
+         var done = false;
+
+         while (!done)
+         {
+            Console.WriteLine("Enter a grade or 'q' to quit");
+            var input = Console.ReadLine();
+
+            if (input == "q")
+            {
+               done = true;
+               continue;
+            }
+
+            var grade = double.Parse(input);
+            book.AddGrade(grade);
+         }
+         // code
+
          var stats = book.GetStatistics();
+
          Console.WriteLine($"The lowest grade is {stats.Low}");
          Console.WriteLine($"The highest grade is {stats.High}");
          Console.WriteLine($"The average grade is {stats.Average:N1}");
-         Console.WriteLine($"The letter grade is{stats.Letter}");
+         Console.WriteLine($"The letter grade is {stats.Letter}");
       }
    }
 }
