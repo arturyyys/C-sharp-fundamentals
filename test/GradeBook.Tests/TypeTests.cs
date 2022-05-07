3da70b876d445900710162bf29ab78469c56a6c8
arturyyys <arturyyys@gmail.com>
2022-05-07 19:02:03 +0300

    Using Multi-cast Delegates

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -7,20 +7,29 @@ public delegate string WriteLogDelegate(string logMessage);
 
 public class TypeTests
 {
+   int count = 0;
 
    [Fact]
    public void WriteLogDelegateCanPointToMethod()
    {
-      WriteLogDelegate log;
+      WriteLogDelegate log = ReturnMessage;
 
-      log = ReturnMessage;
+      log += ReturnMessage;
+      log += IncrementCount;
 
       var result = log("Hello!");
-      Assert.Equal("Hello!", result);
+      Assert.Equal(3, count);
+   }
+
+   string IncrementCount(string message)
+   {
+      count++;
+      return message.ToLower();
    }
 
    string ReturnMessage(string message)
    {
+      count++;
       return message;
    }
 
