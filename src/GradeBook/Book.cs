ed363ac6c4f5ea1136bf223d3a375de27283c543
arturyyys <arturyyys@gmail.com>
2022-05-07 17:02:08 +0300

    Defining a Delegate

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -97,7 +97,7 @@ namespace GradeBook
       public string Name
       {
          get;
-         private set;
+         set;
       }
 
       public const string CATEGORY = "Science";
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -3,8 +3,26 @@ using Xunit;
 
 namespace GradeBook.Tests;
 
+public delegate string WriteLogDelegate(string logMessage);
+
 public class TypeTests
 {
+
+   [Fact]
+   public void WriteLogDelegateCanPointToMethod()
+   {
+      WriteLogDelegate log;
+
+      log = ReturnMessage;
+
+      var result = log("Hello!");
+      Assert.Equal("Hello!", result);
+   }
+
+   string ReturnMessage(string message)
+   {
+      return message;
+   }
 
    [Fact]
    public void Test1()
