64d3de6b58b82569a082901b635f07dbade1db20
arturyyys <arturyyys@gmail.com>
2022-05-11 19:58:34 +0300

    Defining an interface

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -16,13 +16,28 @@ namespace GradeBook
       }
    }
 
-   public abstract class Book : NamedObject
+   public interface IBook
+   {
+      void AddGrade(double grade);
+      Statistics GetStatistics();
+      string Name { get; }
+      event GradeAddedDelegate GradeAdded;
+   }
+
+   public abstract class Book : NamedObject, IBook
    {
       protected Book(string name) : base(name)
       {
       }
 
+      public virtual event GradeAddedDelegate GradeAdded;
+
       public abstract void AddGrade(double grade);
+
+      public virtual Statistics GetStatistics()
+      {
+         throw new NotImplementedException();
+      }
    }
 
    public class InMemoryBook : Book
@@ -71,9 +86,9 @@ namespace GradeBook
          }
       }
 
-      public event GradeAddedDelegate GradeAdded;
+      public override event GradeAddedDelegate GradeAdded;
 
-      public Statistics GetStatistics()
+      public override Statistics GetStatistics()
       {
          var result = new Statistics();
          result.Average = 0.0;
