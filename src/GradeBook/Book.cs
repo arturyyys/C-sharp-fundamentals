95a74ea7afa4c348b28c46fa857fa60ad0ce3f4a
arturyyys <arturyyys@gmail.com>
2022-05-12 21:17:26 +0300

    Using IDisposable

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -45,9 +45,14 @@ namespace GradeBook
 
       public override void AddGrade(double grade)
       {
-         var writer = File.AppendText($"{Name}.txt");
-         writer.WriteLine(grade);
-         writer.Close();
+         using (var writer = File.AppendText($"{Name}.txt"))
+         {
+            writer.WriteLine(grade);
+            if (GradeAdded != null)
+            {
+               GradeAdded(this, new EventArgs());
+            }
+         }
       }
 
       public override Statistics GetStatistics()
