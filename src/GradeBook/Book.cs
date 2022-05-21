b61d9a417f3678e35acd6d88342fc179fb660d62
arturyyys <arturyyys@gmail.com>
2022-05-21 22:31:02 +0300

    Refactoring Statistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -57,7 +57,19 @@ namespace GradeBook
 
       public override Statistics GetStatistics()
       {
-         throw new NotImplementedException();
+         var result = new Statistics();
+
+         using (var reader = File.OpenText($"{Name}.txt"))
+         {
+            var line = reader.ReadLine();
+            while (line != null)
+            {
+               var number = double.Parse(line);
+               result.Add(number);
+               line = reader.ReadLine();
+            }
+         }
+         return result;
       }
    }
 
@@ -112,9 +124,6 @@ namespace GradeBook
       public override Statistics GetStatistics()
       {
          var result = new Statistics();
-         result.Average = 0.0;
-         result.High = double.MinValue;
-         result.Low = double.MaxValue;
 
          var book = new InMemoryBook("Scott's Grade Book");
          book.AddGrade(89.1);
@@ -127,35 +136,8 @@ namespace GradeBook
             {
                break;
             }
-            result.Low = Math.Min(grades[index], result.Low);
-            result.High = Math.Max(grades[index], result.High);
-            result.Average += grades[index];
+            result.Add(grades[index]);
          };
-
-         result.Average /= grades.Count;
-
-         switch (result.Average)
-         {
-            case var d when d >= 90.0:
-               result.Letter = 'A';
-               break;
-
-            case var d when d >= 80.0:
-               result.Letter = 'B';
-               break;
-
-            case var d when d >= 70.0:
-               result.Letter = 'C';
-               break;
-
-            case var d when d >= 60.0:
-               result.Letter = 'D';
-               break;
-
-            default:
-               result.Letter = 'F';
-               break;
-         }
 
          return result;
       }
